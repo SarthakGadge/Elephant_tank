5ccd8bab407d68b114b7d4d14ab7d0095744cea7
Sarthak  Gadge <sarthak.gadge@tdtl.world>
2025-01-09 15:18:05 +0530

    9 Jan change-4

--- form/urls.py
+++ form/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import serializers, viewsets, routers
-from form.views import ApplicantViewSet
+from form.views import ApplicantViewSet, GetApplicants
 
 
 router = routers.DefaultRouter()
@@ -8,4 +8,5 @@ router.register(r'applicants', ApplicantViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('all_applicants/', GetApplicants.as_view(), name='GetApplicants'),
 ]
--- form/views.py
+++ form/views.py
@@ -2,8 +2,23 @@ from rest_framework import viewsets
 from .models import Applicant  # Import the Applicant model from the current app
 from .Serializers import ApplicantSerializer  # Import the ApplicantSerializer from the current app
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
 
 class ApplicantViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
     queryset = Applicant.objects.all()
     serializer_class = ApplicantSerializer
+    
+class GetApplicants(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request):
+        try:
+            instance = Applicant.objects.all()
+            serializer = ApplicantSerializer(instance, many=True)
+            return Response({"msg":serializer.data}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"msg":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            
